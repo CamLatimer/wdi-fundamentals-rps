126f46817eec3ec8d2ad92978cc89eddd37564e8
Cam <camlatimer@gmail.com>
2015-12-31 14:21:19 -0500

    finished unit 4 homework on conditionals and loops

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -41,6 +41,19 @@ function getWinner(playerMove,computerMove) {
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
+    if (playerMove === computerMove) {
+        winner = 'tie';
+    } else if (playerMove === 'rock' && computerMove == 'scissors') {
+        winner = 'player';
+    } else if (playerMove == 'scissors' && computerMove == 'rock') {
+        winner = 'computer';
+    } else if (playerMove == 'paper' && computerMove == 'scissors') {
+        winner = 'computer';
+    } else if (playerMove == 'scissors' && computerMove == 'paper') {
+        winner = 'player';
+    } else {
+        return 'choose again';
+    }
     return winner;
 }
 
